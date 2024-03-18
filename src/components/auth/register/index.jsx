d8242db1e350f460492cf444aaab92fc293d02d8
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-03-18 12:03:44 +0000

    Improves styling in login/register pages

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -1,108 +1,116 @@
-import React, { useState } from 'react'
-import { Navigate, Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../../utils/contexts/authContext'
-import { doCreateUserWithEmailAndPassword } from '../../../config/auth'
-import { db } from '../../../config/firebase'
-import { createUserDocument } from '../../../config/firestore'
+import React, { useState } from "react";
+import { Navigate, Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../../utils/contexts/authContext";
+import { doCreateUserWithEmailAndPassword } from "../../../config/auth";
+import { db } from "../../../config/firebase";
+import { createUserDocument } from "../../../config/firestore";
+import "../login.css";
 
 const Register = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setconfirmPassword] = useState('')
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
+  const { userLoggedIn } = useAuth();
 
-    const { userLoggedIn } = useAuth()
-
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        if(!isRegistering) {
-            setIsRegistering(true)
-            const { user } = await doCreateUserWithEmailAndPassword(email, password);
-            await createUserDocument(user);
-        }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!isRegistering) {
+      setIsRegistering(true);
+      const { user } = await doCreateUserWithEmailAndPassword(email, password);
+      await createUserDocument(user);
     }
+  };
 
-    return (
-        <>
-            {userLoggedIn && (<Navigate to={'/home'} replace={true} />)}
+  return (
+    <>
+      {userLoggedIn && <Navigate to={"/home"} replace={true} />}
 
-            <main className="w-full h-screen flex self-center place-content-center place-items-center">
-                <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
-                    <div className="text-center mb-6">
-                        <div className="mt-2">
-                            <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
-                        </div>
+      <main className="loginCompWrapper">
+        <div className="loginWrapper">
+          <h3 className="login-header">Create a New Account</h3>
 
-                    </div>
-                    <form
-                        onSubmit={onSubmit}
-                        className="space-y-4"
-                    >
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                autoComplete='email'
-                                required
-                                value={email} onChange={(e) => { setEmail(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+          <form onSubmit={onSubmit} className="login-form">
+            <div className="d-flex flex-column">
+              <div className="d-flex flex-column">
+                <label className="login-label">Email</label>
+                <input
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  className="login-input"
+                />
+              </div>
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='new-password'
-                                required
-                                value={password} onChange={(e) => { setPassword(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+              <div className="d-flex flex-column">
+                <label className="login-label">Password</label>
+                <input
+                  disabled={isRegistering}
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  className="login-input"
+                />
+              </div>
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Confirm Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='off'
-                                required
-                                value={confirmPassword} onChange={(e) => { setconfirmPassword(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+              <div className="d-flex flex-column">
+                <label className="login-label">Confirm Password</label>
+                <input
+                  disabled={isRegistering}
+                  type="password"
+                  autoComplete="off"
+                  required
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setconfirmPassword(e.target.value);
+                  }}
+                  className="login-input"
+                />
+              </div>
 
-                        {errorMessage && (
-                            <span className='text-red-600 font-bold'>{errorMessage}</span>
-                        )}
+              <button
+                type="submit"
+                disabled={isRegistering}
+                className={`login-submitBtn align-self-center align-self-sm-end mt-4 mt-sm-0 rounded  ${
+                  isRegistering
+                    ? "bg-gray-300 cursor-not-allowed"
+                    : "bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300"
+                }`}
+              >
+                {isRegistering ? "Signing Up..." : "Sign Up"}
+              </button>
+            </div>
 
-                        <button
-                            type="submit"
-                            disabled={isRegistering}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
-                        >
-                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                        </button>
-                        <div className="text-sm text-center">
-                            Already have an account? {'   '}
-                            <Link to={'/login'} className="text-center text-sm hover:underline font-bold">Continue</Link>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </>
-    )
-}
+            {errorMessage && (
+              <span className="text-red-600 font-bold">{errorMessage}</span>
+            )}
 
-export default Register
+            <div className="mt-4 text-center login-smallPar">
+              Already have an account? {"   "}
+              <Link
+                to={"/login"}
+                className="text-center text-sm hover:underline font-bold"
+              >
+                Continue
+              </Link>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Register;
